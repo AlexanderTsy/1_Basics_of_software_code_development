8bc9ddbce35296b9451d53b1967fee2620869088
Alexandr Tsyhanok <shurius@gmail.com>
2022-03-15 16:52:07 +0300

    Add branching Task2

---
+++ src/by/jonline/basics01/branching/Task2.java
@@ -0,0 +1,23 @@
+package by.jonline.basics01.branching;
+
+public class Task2 {
+	// Ветвления. Задание 2
+	// Найти max{min(a,b), min(c,d)}
+	
+	// Запишем исходные данные в теле программы
+	private static double a = -150.5846054;
+	private static double b = 56048.0;
+	private static double c = 12;
+	private static double d = 64;
+	
+	public static void main(String[] args) {
+		//запишем в x min(a,b)
+		double x = a<b?a:b;
+		//в y min(c,d)
+		double y = c<d?c:d;
+		// max{x,y}
+		double result = x>y?x:y;
+		System.out.println(String.format("max{min(%s,%s), min(%s,%s)} равно: %s", a,b,c,d,result));
+	}
+
+}
