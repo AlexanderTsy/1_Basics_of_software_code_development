8781ac39d009c6081ed315e69742e334d4b9047c
Alexandr Tsyhanok <shurius@gmail.com>
2022-03-16 16:07:36 +0300

    Add loops Task3

---
+++ src/by/jonline/basics01/loops/Task3.java
@@ -0,0 +1,17 @@
+package by.jonline.basics01.loops;
+
+public class Task3 {
+	/* Циклы Задание 3
+	 * Найти сумму квадратов первых ста чисел.
+	 */
+	
+	//предположим что имеется в виду сумма квадратов натуральных чисел от 1 до 100
+	public static void main(String[] args) {
+		int sum=0;
+		for (int i = 1; i <= 100; i++) {
+			sum+=i*i;
+		}
+		System.out.println("Сумма квадратов первых ста чисел равна:"+sum);
+	}
+
+}
