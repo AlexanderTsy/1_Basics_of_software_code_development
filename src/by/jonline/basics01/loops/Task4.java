aa785c444ce2c4178f9826b52e871985a3272176
Alexandr Tsyhanok <shurius@gmail.com>
2022-03-16 16:43:43 +0300

    Add loops Task4

---
+++ src/by/jonline/basics01/loops/Task4.java
@@ -0,0 +1,25 @@
+package by.jonline.basics01.loops;
+
+import java.math.BigInteger;
+
+public class Task4 {
+	/* Циклы. Задание 4
+	 * Составить программу нахождения произведения квадратов первых двухсот чисел.
+	 */
+	
+	// Рассчитаем произведение квадратов натуральных чисел от 1 до 200
+	public static void main(String[] args) {
+		//Поскольку искомое значение предположительно превысит диапазон типа long используем тип BigInteger
+		BigInteger product;
+		product = BigInteger.ONE;
+		int i=1;
+		while (i<=200) {
+			product=product.multiply(BigInteger.valueOf(i*i));
+			i++;
+		}
+		System.out.println("Произведение квадратов первых двухсот чисел равно:\r\n"+product);
+		System.out.println("Количество знаков числа: "+product.toString().length());
+
+	}
+
+}
