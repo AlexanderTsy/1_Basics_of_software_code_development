49c549ddd04b8ad5b248523453e9705846c3f3e1
Alexandr Tsyhanok <shurius@gmail.com>
2022-03-16 17:48:34 +0300

    Add loops Task6

---
+++ src/by/jonline/basics01/loops/Task6.java
@@ -0,0 +1,30 @@
+package by.jonline.basics01.loops;
+
+public class Task6 {
+	/* Циклы. Задание 6
+	 * Вывести на экран соответствий между символами и их численными обозначениями в памяти компьютера.
+	 */
+	
+	/*
+	 * в Java тип char отображает символы в кодировке utf-8
+	 * размер типа 2 байта, может принимать 65536 значений
+	 * для целей задачи выведем соответствие символов их значениям между начальным и конечным номером
+	 * например символы русского алфавита
+	 */
+	public static void main(String[] args) {
+		int startNum=0x0410; //начальный номер
+		int endNum=0x044F; //конечный номер
+		int number=startNum; // переменная с текущим номером
+		
+		do {
+			System.out.println("'"+(char)number+"' - "+number);
+			number++;
+		} while (number<=endNum);
+		// еще немного интересных букв
+		System.out.println("'"+(char)0x0406+"' - "+number);
+		System.out.println("'"+(char)0x0456+"' - "+number);
+		System.out.println("'"+(char)0x040E+"' - "+number);
+		System.out.println("'"+(char)0x045E+"' - "+number);
+	}
+
+}
