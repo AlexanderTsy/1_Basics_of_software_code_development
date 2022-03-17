3769134baa675421547eaa38743e62e80201e780
Alexandr Tsyhanok <shurius@gmail.com>
2022-03-17 14:35:18 +0300

    Fix closing System.in in loops Task1

--- src/by/jonline/basics01/loops/Task1.java
+++ src/by/jonline/basics01/loops/Task1.java
@@ -4,15 +4,18 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Task1 {
-	/* Циклы. Здание 1
-	 * Напишите программу, где пользователь вводит любое целое положительное число.
-	 * А программа суммирует все числа от 1 до введенного пользователям числа.
+	/*
+	 * Циклы. Здание 1 Напишите программу, где пользователь вводит любое целое
+	 * положительное число. А программа суммирует все числа от 1 до введенного
+	 * пользователям числа.
 	 */
-	
-	/* Ограничим пользоавтельский ввод типом int, максимальное число 2 147 483 647.
+
+	/*
+	 * Ограничим пользоавтельский ввод типом int, максимальное число 2 147 483 647.
 	 * результат вычислений сохраним в тип long.
 	 */
 	public static void main(String[] args) {
+		@SuppressWarnings("resource")
 		Scanner scanner = new Scanner(System.in);
 		int number = 0;
 		boolean isNumberOk = false;
@@ -20,7 +23,7 @@ public class Task1 {
 			System.out.println("Введите положительное целое число, не более 2147483647");
 			try {
 				number = scanner.nextInt();
-				if (number>0 && number<=2147483647) {
+				if (number > 0 && number <= 2147483647) {
 					isNumberOk = true;
 				} else {
 					throw new InputMismatchException();
@@ -32,11 +35,9 @@ public class Task1 {
 		} while (!isNumberOk);
 		long sum = 0;
 		for (int i = 1; i <= number; i++) {
-			sum+=i;
+			sum += i;
 		}
-		System.out.println("Сумма чисел от 1 до "+number+" равна "+sum);
-		scanner.close();
-
+		System.out.println("Сумма чисел от 1 до " + number + " равна " + sum);
 	}
 
 }
