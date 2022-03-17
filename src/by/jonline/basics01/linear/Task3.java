aef7a8f7c1056d4826947a3f0e53d95c1e3b80ea
Alexandr Tsyhanok <shurius@gmail.com>
2022-03-17 14:30:32 +0300

    Fix closing System.in in linear Task3

--- src/by/jonline/basics01/linear/Task3.java
+++ src/by/jonline/basics01/linear/Task3.java
@@ -8,26 +8,28 @@ import java.util.Scanner;
 
 public class Task3 {
 	public static void main(String[] args) {
-		//Вычислить значение выражения по формуле (все переменные принимают действительные значения):
+		// Вычислить значение выражения по формуле (все переменные принимают
+		// действительные значения):
 		// (sin x + cos y)/(cos x - sin y)*tg(xy)
-		// Консольное приложение вычисляет значение выражения с помощью пакета java.lang.Math, значения x и y пользователь вводит с клавиатуры
+		// Консольное приложение вычисляет значение выражения с помощью пакета
+		// java.lang.Math, значения x и y пользователь вводит с клавиатуры
 		double x;
 		double y;
 		double result;
-		//создадим объект типа java.util.Scanner для считывания пользовательского ввода
+
+		// создадим объект типа java.util.Scanner для считывания пользовательского ввода
+		@SuppressWarnings("resource")
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.println("Будет вычислено значение выражения (sin x + cos y)/(cos x - sin y)*tg(xy)");
 		System.out.println("Введите значение х (действительное число)");
 		x = scanner.nextDouble();
 		System.out.println("Введите значение y (действительное число)");
 		y = scanner.nextDouble();
-		
-		result = (sin(x)+cos(y))/(cos(x)-sin(y))*tan(x*y);
-		
-		System.out.println("значение выражения: "+result);
-		
-		scanner.close();
-		
+
+		result = (sin(x) + cos(y)) / (cos(x) - sin(y)) * tan(x * y);
+
+		System.out.println("значение выражения: " + result);
+
 	}
 }
