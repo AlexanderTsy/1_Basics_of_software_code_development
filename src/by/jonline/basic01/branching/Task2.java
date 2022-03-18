ed0d9473a913aba244c43b1d0513e53c1c8e8be1
Alexandr Tsyhanok <shurius@gmail.com>
2022-03-18 17:37:35 +0300

    Refactoring. New method findMaxOfMin(a,b,c,d)

--- src/by/jonline/basic01/branching/Task2.java
+++ src/by/jonline/basic01/branching/Task2.java
@@ -4,20 +4,32 @@ public class Task2 {
 	// Ветвления. Задание 2
 	// Найти max{min(a,b), min(c,d)}
 	
-	// Запишем исходные данные в теле программы
-	private static double a = -150.5846054;
-	private static double b = 56048.0;
-	private static double c = 12;
-	private static double d = 64;
-	
 	public static void main(String[] args) {
+	
+		// Тестовые данные 1
+		double a = -150.5846054;
+		double b = 56048.0;
+		double c = 12;
+		double d = 64;
+		System.out.println(String.format("max{min(%s,%s), min(%s,%s)} равно: %s", a,b,c,d,findMaxOfMin(a,b,c,d)));
+		// Тестовые данные 2
+		a = 1;
+		b = 900;
+		c = -1;
+		d = 65;
+		System.out.println(String.format("max{min(%s,%s), min(%s,%s)} равно: %s", a,b,c,d,findMaxOfMin(a,b,c,d)));
+	}
+	
+	/*
+	 * Метод находит max{min(a,b), min(c,d)}
+	 */
+	public static double findMaxOfMin(double a, double b, double c, double d) {
 		//запишем в x min(a,b)
 		double x = a<b?a:b;
-		//в y min(c,d)
+		//запишем в y min(c,d)
 		double y = c<d?c:d;
-		// max{x,y}
-		double result = x>y?x:y;
-		System.out.println(String.format("max{min(%s,%s), min(%s,%s)} равно: %s", a,b,c,d,result));
+		//найдем результат max{x,y}
+		return x>y?x:y;
 	}
 
 }
