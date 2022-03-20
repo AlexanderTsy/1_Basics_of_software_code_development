4f9300daeb2d147411cb82a9db37a3c22cfeb831
Alexandr Tsyhanok <shurius@gmail.com>
2022-03-20 20:56:43 +0300

    upd basic01.branching Task1
    Refactored. Add separate methods to check triangle exists and check
    triangle right. Add tests.

--- src/by/jonline/basic01/branching/Task1.java
+++ src/by/jonline/basic01/branching/Task1.java
@@ -8,42 +8,87 @@ public class Task1 {
 	// треугольник,
 	// и если да, то будет ли он прямоугольным.
 
-	// Предположим что пользователь вводит через пробел значения двух углов,
+	public static boolean checkTriangleExist(float angle1, float angle2) {
+		/*
+		 * Сумма углов треугольника равна 180 градусов. Следовательно сумма двух углов
+		 * не превышает 180. Каждый угол больше 0 и меньше 180 градусов.
+		 */
+		return (angle1 > 0 && angle1 < 180 && angle2 > 0 && angle2 < 180 && angle1 + angle2 < 180);
+	}
+	
+	public static boolean checkRightTriangle(float angle1, float angle2) {
+		/* треугольник будет прямоугольным, если один из углов равен 90
+		 * или сумма двух углов равна 90 
+		 */
+		return ( angle1==90 || angle2 == 90 || (angle1 + angle2) == 90);
+	}
+	
+	// Предположим что пользователь вводит величину угла в градусах с клавиатуры,
 	// используем тип float для этих значений
-	public static void main(String[] args) {
-		String input;
-		String[] inputSplit;
-
+	private static float enterFloatFromConsole(String message) {
 		@SuppressWarnings("resource")
 		Scanner scanner = new Scanner(System.in);
-
-		System.out.println("Введите через пробел значения двух углов. Десятичный разделитель точка \".\"");
-		// разделим ввод через пробел
-		input = scanner.nextLine();
-		try {
-			inputSplit = input.split("\\s+");
-			float angle1 = Float.parseFloat(inputSplit[0]);
-			float angle2 = Float.parseFloat(inputSplit[1]);
-			// Каждый угол треугольника больше 0 и меньше 180 градусов.
-			// Сумма углов треугольника равна 180 градусов. Следовательно сумма двух углов
-			// не превышает 180.
-			if (angle1 > 0 && angle1 < 180 && angle2 > 0 && angle2 < 180 && angle1 + angle2 < 180) {
-				System.out.println("Треугольник существует");
-				// Если треугольник прямоугольный, то один из его углов равен 90 градусов,
-				// следовательно сумма оставшихся должна быть равна 90 градусов.
-				if ((angle1 + angle2) == 90.0) {
-					System.out.println("Треугольник является прямоугольным");
-				} else {
-					System.out.println("Треугольник не является прямоугольным");
-				}
-			} else {
-				System.out.println("Треугольник с углами " + input + " не существует.");
-			}
-		} catch (NumberFormatException numExc) {
-			System.err.println("Не удается преобразовать в числа введенные данные. Вы ввели: " + input);
-		} catch (ArrayIndexOutOfBoundsException arrayExc) {
-			System.err.println("Убедитесь, что введенные числа разделены пробелом. Вы ввели: " + input);
+		
+		System.out.println(message);
+		while (!scanner.hasNextFloat()) {
+			scanner.next();
+			System.out.println(message);
 		}
+		return scanner.nextFloat();
+	}
+	
+	// Выводит в консоль описание треугольника с углами angle1 и angle2
+	public static void reportTriangle (float angle1, float angle2) {
+		System.out.println(String.format("Введенные углы %f, %f", angle1, angle2));
+		if (checkTriangleExist(angle1, angle2)) {
+			System.out.println("Треугольник существует");
+			
+			if (checkRightTriangle(angle1, angle2)) {
+				System.out.println("Треугольник является прямоугольным");
+			} else {
+				System.out.println("Треугольник не является прямоугольным");
+			}
+		} else {
+			System.out.println("Треугольник не существует.");
+		}
+	}
+
+	
+	
+	public static void main(String[] args) {
+		float angle1;
+		float angle2;
+		String message="Введите величину угла в градусах от 0 до 180";
+		angle1 = enterFloatFromConsole(message);
+		angle2 = enterFloatFromConsole(message);
+		
+		System.out.println("Данные введены пользователем");
+		reportTriangle(angle1, angle2);
+		
+		System.out.println("Тест 1. Прямоугольный треугольник, один из передаваемых углов прямой"); 
+		angle1 = 90;
+		angle2 = 20;
+		
+		reportTriangle(angle1, angle2);
+		
+		System.out.println("Тест 2. Прямоугольный треугольник, сумма углов 90"); 
+		angle1 = 23.555f;
+		angle2 = 66.445f;
+		
+		reportTriangle(angle1, angle2);
+		
+		System.out.println("Тест 3. Один из углов больше 180"); 
+		angle1 = 181f;
+		angle2 = 15f;
+		
+		reportTriangle(angle1, angle2);
+		
+		System.out.println("Тест 4. Один из углов меньше 0, сумма двух 90"); 
+		angle1 = -10f;
+		angle2 = 100f;
+		
+		reportTriangle(angle1, angle2);
+		
 	}
 
 }
