92cb60d292b4751769495fda6bd905695db0dcde
Alexandr Tsyhanok <shurius@gmail.com>
2022-03-21 13:16:45 +0300

    update basic01.linear Task1
    Add functionZ(a,b,c). Add tests to main

--- src/by/jonline/basic01/linear/Task1.java
+++ src/by/jonline/basic01/linear/Task1.java
@@ -1,22 +1,45 @@
 package by.jonline.basic01.linear;
 
+/*
+ * 1. Basics of software code development
+ * Линейные программы Задание 1
+ * Найдите значение функции: z =((a-3)*b/2)+c
+ */
+
 public class Task1 {
+	
+	/*
+	 *  @return z =((a-3)*b/2)+c
+	 */
+	public static double functionZ(final double a, final double b, final double c) {
+		return ((a-3)*b/2)+c;
+	}
+	
     public static void main(String[] args) {
         //Программа вычисляет значение нижеприведенной функции от трех вещественных аргументов, передаваемых в массиве args
         //и выводит в консоль описание задачи и ответ в последней строке.
-        // Найдите значение функции: z =((a-3)*b/2)+c
+        
         double a;
         double b;
         double c;
 
-        double z; // результат
-        // инициализация переменных
-        a = Double.parseDouble(args[0]);
-        b = Double.parseDouble(args[1]);
-        c = Double.parseDouble(args[2]);
-        //вычисление значения
-        z =((a-3)*b/2)+c;
+        double requiredResult; // результат, который должен быть
+        
+        System.out.println("Значение функции z =((a-3)*b/2)+c");
+        //Тест 1.
+        a = 1;
+        b = 2;
+        c = 3;
+        requiredResult = 1;
         //вывод значения функции
-        System.out.println("Значение функции z =((a-3)*b/2)+c для аргументов \na= "+a+", b="+b+", c= "+c+"\nравно: \n"+z);
+        System.out.println("Для аргументов \na= "+a+", b="+b+", c= "+c+"\nравно: "+functionZ(a, b, c)+" должно быть "+requiredResult);
+        
+      //Тест 2. Переполнение double
+        a = Double.MAX_VALUE;
+        b = 2;
+        c = 3;
+        requiredResult = Double.POSITIVE_INFINITY;
+        //вывод значения функции
+        System.out.println("Для аргументов \na= "+a+", b="+b+", c= "+c+"\nравно: "+functionZ(a, b, c)+" должно быть "+requiredResult);
     }
 }
