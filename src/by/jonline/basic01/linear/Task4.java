91876b683227b3c6189069a312c349a098ab27dd
Alexandr Tsyhanok <shurius@gmail.com>
2022-03-21 15:36:52 +0300

    Update basic01.linear Task4
    Refactoring. Method changeDigits(R)

--- src/by/jonline/basic01/linear/Task4.java
+++ src/by/jonline/basic01/linear/Task4.java
@@ -1,37 +1,56 @@
 package by.jonline.basic01.linear;
-
-import java.util.Scanner;
+/*
+ * 1. Basics of software code development
+ * Линейные программы Задание 4
+ * Дано действительное число R вида nnn.ddd (три цифровых разряда в дробной и целой частях).
+ * Поменять местами дробную и целую части числа и вывести полученное значение числа.
+ */
 
 public class Task4 {
-	// Дано действительное число R вида nnn.ddd (три цифровых разряда в дробной и
-	// целой частях).
-	// Поменять местами дробную и целую части числа и вывести полученное значение
-	// числа.
 
-	// Сделаем допущение, что числа вида 0nn.0dd будут преобразовываться к виду
-	// dd.0nn
+	/*
+	 * Метод принимает действительное число R вида nnn.ddd (три цифровых разряда в
+	 * дробной и целой частях) Возвращает число ddd.nnn Сделаем допущение, что числа
+	 * вида 0nn.0dd будут преобразовываться к виду dd.0nn
+	 * 
+	 * TODO возможно следует решать задачу в строковом представлении, проверяя что nnn и ddd
+	 * являются цифрами разделенными точкой
+	 */
+	public static float changeDigits(final float R) {
+		String rString; // строковое представление числа R
+		rString = Float.toString(R);
+
+		if (rString.length() == 7 && rString.substring(3,4).contentEquals(".")) {
+			return Float.parseFloat(rString.substring(4, 7) + "." + rString.substring(0, 3));
+
+		} else {
+			System.err.println("Не верный формат числа R=" + R + " Должно быть число вида nnn.ddd");
+			return 0;
+		}
+	}
+
 	public static void main(String[] args) {
-		// число запросим у пользователя через консоль.
-		@SuppressWarnings("resource")
-		Scanner scanner = new Scanner(System.in);
-		String input;
-		float result;
+		float r; // число R
 
-		// используем регулярное выражение как паттерн для проверки правильности ввода
-		// пользователя.
-		String pattern = "\\d{3}\\.\\d{3}";
-		do {
-			System.out.println("Введите число R вида nnn.ddd (три цифровых разряда в дробной и целой частях)");
-			input = scanner.next();
-			if (input.matches(pattern)) {// проверяем формат ввода
-				result = Float.parseFloat(input.substring(4, 7) + "." + input.substring(0, 3));// составляем новое чило
-																								// и записываем его в
-																								// переменную result
-				System.out.println(result);
-				break;
-			} else {
-				System.out.println("Ошибка ввода, убедитесь что ввели число вида nnn.ddd");
-			}
-		} while (true);
+		System.out.println("Test 1");
+		r = 111.222f;
+		System.out.println(r + " - " + changeDigits(r));
+		
+		System.out.println("Test 2");
+		r = 123.321f;
+		System.out.println(r + " - " + changeDigits(r));
+				
+		System.out.println("Test 3");
+		r = 011.220f;
+		System.out.println(r + " - " + changeDigits(r));
+		
+		System.out.println("Test 4");
+		r = 000.000f;
+		System.out.println(r + " - " + changeDigits(r));
+		
+		System.out.println("Test 5");
+		r = 100.001f;
+		System.out.println(r + " - " + changeDigits(r));
+				
 	}
 }
